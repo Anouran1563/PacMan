a8ccabdcbb9b09c4c161b623348d41d2a3fb8c31
Anouar Zinedine <anouarzinedine16@gmail.com>
2023-11-09 21:39:56 +0100

    Added controls & included logic

--- Test1/Form1.Designer.cs
+++ Test1/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -38,6 +39,27 @@
             this.pbYellow = new System.Windows.Forms.PictureBox();
             this.pbPink = new System.Windows.Forms.PictureBox();
             this.pbPlayer = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.pictureBox15 = new System.Windows.Forms.PictureBox();
+            this.pictureBox16 = new System.Windows.Forms.PictureBox();
+            this.pictureBox17 = new System.Windows.Forms.PictureBox();
+            this.pictureBox18 = new System.Windows.Forms.PictureBox();
+            this.pictureBox19 = new System.Windows.Forms.PictureBox();
+            this.pictureBox20 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.pictureBox22 = new System.Windows.Forms.PictureBox();
+            this.pictureBox23 = new System.Windows.Forms.PictureBox();
+            this.pictureBox24 = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblOver = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -48,6 +70,24 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbYellow)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPink)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox18)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox23)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox24)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -148,12 +188,199 @@
             // pbPlayer
             // 
             this.pbPlayer.Image = global::Test1.Properties.Resources.right;
-            this.pbPlayer.Location = new System.Drawing.Point(63, 34);
+            this.pbPlayer.Location = new System.Drawing.Point(66, 120);
             this.pbPlayer.Name = "pbPlayer";
             this.pbPlayer.Size = new System.Drawing.Size(46, 48);
             this.pbPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbPlayer.TabIndex = 3;
             this.pbPlayer.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(346, 429);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox6.TabIndex = 4;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "coin";
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(379, 429);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox8.TabIndex = 4;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "coin";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(412, 429);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox9.TabIndex = 4;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "coin";
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(346, 477);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox10.TabIndex = 4;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Tag = "coin";
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(379, 477);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox11.TabIndex = 4;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Tag = "coin";
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Location = new System.Drawing.Point(412, 477);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox12.TabIndex = 4;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Tag = "coin";
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Location = new System.Drawing.Point(346, 518);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox13.TabIndex = 4;
+            this.pictureBox13.TabStop = false;
+            this.pictureBox13.Tag = "coin";
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Location = new System.Drawing.Point(379, 518);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox14.TabIndex = 4;
+            this.pictureBox14.TabStop = false;
+            this.pictureBox14.Tag = "coin";
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.Location = new System.Drawing.Point(412, 518);
+            this.pictureBox15.Name = "pictureBox15";
+            this.pictureBox15.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox15.TabIndex = 4;
+            this.pictureBox15.TabStop = false;
+            this.pictureBox15.Tag = "coin";
+            // 
+            // pictureBox16
+            // 
+            this.pictureBox16.Location = new System.Drawing.Point(313, 31);
+            this.pictureBox16.Name = "pictureBox16";
+            this.pictureBox16.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox16.TabIndex = 4;
+            this.pictureBox16.TabStop = false;
+            this.pictureBox16.Tag = "coin";
+            // 
+            // pictureBox17
+            // 
+            this.pictureBox17.Location = new System.Drawing.Point(346, 31);
+            this.pictureBox17.Name = "pictureBox17";
+            this.pictureBox17.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox17.TabIndex = 4;
+            this.pictureBox17.TabStop = false;
+            this.pictureBox17.Tag = "coin";
+            // 
+            // pictureBox18
+            // 
+            this.pictureBox18.Location = new System.Drawing.Point(313, 79);
+            this.pictureBox18.Name = "pictureBox18";
+            this.pictureBox18.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox18.TabIndex = 4;
+            this.pictureBox18.TabStop = false;
+            this.pictureBox18.Tag = "coin";
+            // 
+            // pictureBox19
+            // 
+            this.pictureBox19.Location = new System.Drawing.Point(346, 79);
+            this.pictureBox19.Name = "pictureBox19";
+            this.pictureBox19.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox19.TabIndex = 4;
+            this.pictureBox19.TabStop = false;
+            this.pictureBox19.Tag = "coin";
+            // 
+            // pictureBox20
+            // 
+            this.pictureBox20.Location = new System.Drawing.Point(313, 120);
+            this.pictureBox20.Name = "pictureBox20";
+            this.pictureBox20.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox20.TabIndex = 4;
+            this.pictureBox20.TabStop = false;
+            this.pictureBox20.Tag = "coin";
+            // 
+            // pictureBox21
+            // 
+            this.pictureBox21.Location = new System.Drawing.Point(346, 120);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox21.TabIndex = 4;
+            this.pictureBox21.TabStop = false;
+            this.pictureBox21.Tag = "coin";
+            // 
+            // pictureBox22
+            // 
+            this.pictureBox22.Location = new System.Drawing.Point(379, 31);
+            this.pictureBox22.Name = "pictureBox22";
+            this.pictureBox22.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox22.TabIndex = 4;
+            this.pictureBox22.TabStop = false;
+            this.pictureBox22.Tag = "coin";
+            // 
+            // pictureBox23
+            // 
+            this.pictureBox23.Location = new System.Drawing.Point(379, 79);
+            this.pictureBox23.Name = "pictureBox23";
+            this.pictureBox23.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox23.TabIndex = 4;
+            this.pictureBox23.TabStop = false;
+            this.pictureBox23.Tag = "coin";
+            // 
+            // pictureBox24
+            // 
+            this.pictureBox24.Location = new System.Drawing.Point(379, 120);
+            this.pictureBox24.Name = "pictureBox24";
+            this.pictureBox24.Size = new System.Drawing.Size(27, 31);
+            this.pictureBox24.TabIndex = 4;
+            this.pictureBox24.TabStop = false;
+            this.pictureBox24.Tag = "coin";
+            // 
+            // lblScore
+            // 
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(84, 32);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // lblOver
+            // 
+            this.lblOver.Font = new System.Drawing.Font("Microsoft Uighur", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOver.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblOver.Location = new System.Drawing.Point(237, 9);
+            this.lblOver.Name = "lblOver";
+            this.lblOver.Size = new System.Drawing.Size(239, 67);
+            this.lblOver.TabIndex = 5;
+            this.lblOver.Text = "GAME OVER";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
             // 
             // Form1
             // 
@@ -161,6 +388,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(684, 561);
+            this.Controls.Add(this.lblOver);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureBox24);
+            this.Controls.Add(this.pictureBox15);
+            this.Controls.Add(this.pictureBox23);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox22);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox21);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox20);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox19);
+            this.Controls.Add(this.pictureBox18);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox17);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox16);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pbPink);
             this.Controls.Add(this.pictureBox7);
             this.Controls.Add(this.pbPlayer);
@@ -173,6 +420,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -183,6 +431,24 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbYellow)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPink)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox18)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox19)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox23)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox24)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -199,6 +465,27 @@
         private System.Windows.Forms.PictureBox pbYellow;
         private System.Windows.Forms.PictureBox pbPink;
         private System.Windows.Forms.PictureBox pbPlayer;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox pictureBox15;
+        private System.Windows.Forms.PictureBox pictureBox16;
+        private System.Windows.Forms.PictureBox pictureBox17;
+        private System.Windows.Forms.PictureBox pictureBox18;
+        private System.Windows.Forms.PictureBox pictureBox19;
+        private System.Windows.Forms.PictureBox pictureBox20;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.PictureBox pictureBox22;
+        private System.Windows.Forms.PictureBox pictureBox23;
+        private System.Windows.Forms.PictureBox pictureBox24;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblOver;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
 
--- Test1/Form1.cs
+++ Test1/Form1.cs
@@ -1,20 +1,150 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using System;
 
 namespace Test1
 {
     public partial class Form1 : Form
     {
+        bool up;
+        bool down;
+        bool left;
+        bool right;
+
+        int speed = 8;
+        int ghost1 = 8;
+        int ghost2 = 8;
+        int ghost3x = 8;
+        int ghost3y = 8;
+        int score = 0;
         public Form1()
         {
             InitializeComponent();
+            lblOver.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyisDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                left = true;
+                pbPlayer.Image = Properties.Resources.left;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                right = true;
+                pbPlayer.Image = Properties.Resources.right;
+            }
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                up = true;
+                pbPlayer.Image = Properties.Resources.Up;
+            }
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                down = true;
+                pbPlayer.Image = Properties.Resources.down;
+            }
+        }
+
+        private void KeyisUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                left = false;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                right = false;
+            }
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                up = false;
+            }
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                down = false;
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            lblScore.Text = "Score: " + score;
+            if (left)
+            {
+                pbPlayer.Left -= speed;
+            }
+            if (right)
+            {
+                pbPlayer.Left += speed;
+            }
+            if (up)
+            {
+                pbPlayer.Top -= speed;
+            }
+            if (down)
+            {
+                pbPlayer.Top += speed;
+            }
+            pbRed.Left += ghost1;
+            pbYellow.Left += ghost2;
+            if (pbRed.Bounds.IntersectsWith(pictureBox4.Bounds))
+            {
+                ghost1 = -ghost1;
+            }
+            else if (pbRed.Bounds.IntersectsWith(pictureBox3.Bounds))
+            {
+                ghost1 = -ghost1;
+            }
+            if (pbYellow.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                ghost2 = -ghost2;
+            }
+            else if (pbYellow.Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+                ghost2 = -ghost2;
+            }
+            foreach (Control x in pbPlayer.Controls)
+            {
+                if (x is PictureBox && x.Tag == "wall" || x.Tag == "ghost")
+                {
+                    if (((PictureBox)x).Bounds.IntersectsWith(pbPlayer.Bounds) || score == 30)
+                    {
+                        pbPlayer.Left = 0;
+                        pbPlayer.Top = 25;
+                        lblOver.Text = "Game Over";
+                        lblOver.Visible = true;
+                        GameTimer.Stop();
+                    }
+                }
+                if (x is PictureBox && x.Tag == "coin")
+                {
+                    if (((PictureBox)x).Bounds.IntersectsWith(pbPlayer.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        score++;
+                    }
+                }
+            }
+            pbPink.Left += ghost3x;
+            pbPink.Top += ghost3y;
+            if (pbPink.Left < 1 ||
+                pbPink.Left + pbPink.Width > ClientSize.Width - 2 ||
+                (pbPink.Bounds.IntersectsWith(pictureBox4.Bounds)) ||
+                (pbPink.Bounds.IntersectsWith(pictureBox3.Bounds)) ||
+                (pbPink.Bounds.IntersectsWith(pictureBox1.Bounds)) ||
+                (pbPink.Bounds.IntersectsWith(pictureBox2.Bounds)))
+            {
+                ghost3x = -ghost3x;
+            }
+            if (pbPink.Top < 1 || pbPink.Top + pbPink.Height > ClientSize.Height - 2)
+            {
+                ghost3y = -ghost3y;
+            }
         }
     }
 }
